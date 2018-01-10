c453519c890545915457ef37bc0363fb0143370b
Mickaël Wolff <mickael@lupusmic.org>
2018-01-10 15:37:07 +0100

    Xp assets

--- xp.js
+++ xp.js
@@ -1,9 +1,353 @@
-assets.jobs =
+assets.xp =
 	[
+		/*
 		{ 'period': { 'start': '', 'end': ''}
 		, 'company': {}
 		, 'role': {}
 		, 'missions': []
 		}
 	,
+	*/
+		{ 'period': { 'start': 'Mars 2015', 'end': 'Mai 2015'}
+		, 'company':
+			{ 'name': 'Rapid Ratings'
+			, 'location':
+				{ 'city': 'Dublin'
+				, 'country': 'Irlande'
+				}
+			, 'business': 'Notation de la santé financière d\'entités et mesure du risque'
+			}
+		, 'role': 'Développeur'
+		, 'skills':
+			[ 'PHP5'
+			, 'Python'
+			, 'Django'
+			, 'PostgreSQL'
+			, 'JavaScript'
+			, 'jQuery'
+			, 'Twitter Bootstrap'
+			, 'Salt'
+			, 'Vagrant'
+			, 'Git'
+			, 'GitHub'
+			, 'Jira Confluence'
+			]
+
+		, 'missions':
+			[ 'maintenance corrective de l\'application historique écrite en PHP'
+			, 'développement de microservices destinés à remplacer des portions de l\'application historique'
+			]
+		}
+	,
+		{ 'period': { 'start': 'Juin 2013', 'end': 'Mars 2015'}
+		, 'company':
+			{ 'name': ' 3V Transaction Services, Safe Charge'
+			, 'location':
+				{ 'city': 'Dublin'
+				, 'country': 'Irlande'
+				}
+			, 'business': 'Éditeur de systèmes de gestion de cartes cadeau et de cartes de paiement prépayées'
+			}
+		, 'role': 'Développeur'
+		, 'skills':
+			['PHP5'
+			, 'Zend Frameword 1 and 2'
+			, 'Symfony 2'
+			, 'Phing'
+			, 'BeHat'
+			, 'Composer'
+			, 'PHPUnit'
+			, 'Oauth 1.0a'
+			, 'JavaScript'
+			, 'jQuery UI'
+			, 'jQuery Mobile'
+			, 'Dojo'
+			, 'Subversion'
+			, 'Git'
+			, 'Jira'
+			]
+		, 'missions':
+			[ 
+			, 'MoneyButton (StarsCard, OrangeCash, DNA Tapaka projects)'
+			, 'développement d\'outils de déploiement en script shell utilisant PHP composer et les capacité de branchage de Subversion'
+			, 'développement de points d\'entrée de l\'API du projet StarsCard'
+			, 'développement du composant de communication du frontend avec l\'API dans l\'architecture MoneyButton'
+			, 'développement de fonctionnalités dans les frontends spécialisés'
+			, 'intégration de design graphiques'
+			, 'O2 Money: maintenance et implémentation de nouvelles fonctionnalités (bare PHP4)'
+			, 'VISA showcase: amélioration et développement de nouvelles fonctionnalités (Zend 1)'
+			, 'portail du service client (Dojo)'
+			, 'écriture de tests comportementaux pour tester l\'API (BeHat)'
+			, 'amélioration et évolution des scripts d\'exportation des bases de données destinées au service de renseignement commercial (business intelligence)'
+			]
+		}
+	,
+		{ 'period': { 'start': 'Oct 2012', 'end': 'Mai 2013'}
+		, 'company':
+			{ 'name': 'Dedsert'
+			, 'location':
+				{ 'city': 'Sandyford'
+				, 'country': 'Irlande'
+				}
+			, 'business': 'Éditeur de sites de paris en ligne'
+			}
+		, 'role': 'Développeur'
+		, 'skills':
+			[ 'Python' 
+			, 'Bottle' 
+			, 'PHP5' 
+			, 'CodeIgniter' 
+			, 'MySQL' 
+			, 'Git'
+			]
+		, 'missions':
+			[ 'mise en place du squelette du frontend de Novaball.com‘s  à destination des intégrateurs'
+			, 'développement du backend JSON REST'
+			, 'gestion de l\'envoie des SMS'
+			, 'gestion des paiement à travers l\'API du fournisseur Paypoint'
+			, 'gestion de la transmission de la liste des joueurs auprès de l\'assureur'
+			]
+		}
+	,
+		{ 'period': { 'start': 'Août 2011', 'end': 'Juin 2012'}
+		, 'company':
+			{ 'name': 'Ooblada'
+			, 'location':
+				{ 'city': 'Paris'
+				, 'country': 'France'
+				}
+			, 'business': 'Éditeur de jeux pour Facebook'
+			}
+		, 'role': 'Développeur'
+		, 'skills':
+			[ 'PHP5' 
+			, 'MySQL' 
+			, 'Memcached' 
+			, 'Javascript'
+			]
+		, 'missions':
+			[ 'améliorations du backend (Treasure Madness, SocialPeopleZ)'
+			, 'Graph API : adaptation des jeux aux nouvelles politiques de gestion des données privées en utilisant les « real time updates »'
+			, 'OpenGraph : intégration des OG objects à Treasure Madness (ticker events pour les achievements) et SocialPeopleZ (scoring)'
+			, 'refactoring du code historique'
+			, 'amélioration de la couverture des tests unitaires (framework maison)'
+			]
+		}
+	,
+		{ 'period': { 'start': 'Mai 2011', 'end': 'Août 2011'}
+		, 'company':
+			{ 'name': 'Profeleve.com'
+			, 'location':
+				{ 'city': 'Paris'
+				, 'country': 'France'
+				}
+			, 'business': 'Annuaire en ligne de professeurs'
+			}
+		, 'role': 'Développeur'
+		, 'skills':
+			[ 'PHP5' 
+			, 'MySQL' 
+			, 'Zend' 
+			, 'Framework' 
+			, 'jQuery'
+			]
+		, 'missions':
+			[ 'mise en place du projet : hébergement, LAMP, dépôt git, Trac'
+			, 'prototypage du site web'
+			, 'recrutement d\'un intégrateur graphique'
+			]
+		}
+	,
+		{ 'period': { 'start': 'Juin 2010', 'end': 'Février 2011'}
+		, 'company':
+			{ 'name': 'Jolt Online Gaming'
+			, 'location':
+				{ 'city': 'Dublin'
+				, 'country': 'Irlande'
+				}
+			, 'business': 'Éditeur de jeux web'
+			}
+		, 'role': 'Développeur'
+		, 'skills':
+			[ 'MongoDB' 
+			, 'Cassandra' 
+			, 'Memcached' 
+			, 'MySQL' 
+			, 'Pylons' 
+			, 'JQuery'
+			]
+			/* XXX Translate XXX */
+		, 'missions':
+			[ 'développement d\'un site de prise de paris sportif (Django)'
+			, 'writing a admin interface to manage player\'s realms in “Playboy Party Manager”'
+			, 'writing the game designer interface of “Championship Manager: Rivals”'
+			, 'benchmarking high load usage to ensure game scalability'
+			]
+		}
+		,
+		{ 'period': { 'start': 'Fev. 2010', 'end': 'Avril 2010'}
+		, 'company':
+			{ 'name': 'Global Electronic Business'
+			, 'location':
+				{ 'city': 'Paris'
+				, 'country': 'France'
+				}
+			, 'business': 'Éditeur du site AdopteUnMec.com'
+			}
+		, 'role': 'Développeur'
+		, 'skills':
+			[ 'PHP5' 
+			, 'MySQL' 
+			, 'JQuery' 
+			, 'Prototype' 
+			, 'Memcached' 
+			, 'Lighttpd'
+			]
+		, 'missions':
+			[ 'correction de bugs'
+			, 'réécriture des formulaire de paiement et d\'inscription'
+			, 'refactoring de l\'authentification et de la gestion des utilisateurs'
+			, 'integration du système de paiement Paybox'
+			]
+		}
+	,
+		{ 'period': { 'start': 'Jan. 2010', 'end': 'Jan. 2010'}
+		, 'company':
+			{ 'name': 'Equinoxe Media'
+			, 'location':
+				{ 'city': 'Paris'
+				, 'country': 'France'
+				}
+			, 'business': 'Éditeur de sites de divertissement (porno, gossip, dating, buzz média)'
+			}
+		, 'role': 'Développeur'
+		, 'skills':
+			[ 'PHP5' 
+			, 'MySQL' 
+			, 'JQuery' 
+			, 'Memcache'
+			]
+		, 'missions':
+			[ 'intégration de contenu publicitaire sur Abrutis.com'
+			, 'développement d\'un service web (JSON) pour une application sur iPhone'
+			]
+		}
+	,
+		{ 'period': { 'start': 'Mai 2009', 'end': 'Nov. 2009'}
+		, 'company':
+			{ 'name': 'Nexen Services (Alterway)'
+			, 'location':
+				{ 'city': 'Colombes'
+				, 'country': 'France'
+				}
+			, 'business': ' Services d\'hébergement internet'
+			}
+		, 'role': 'Développeur'
+		, 'skills':
+			[ 'PHP4' 
+			, 'MySQL'
+			]
+		, 'missions':
+			[ 'développement de maintenant PHP4/MySQL4'
+			, 'étude de d\'impact et de faisabilité de la migration du SI de PHP4 vers PHP5'
+			, 'formateur PHP pour Anaska (Alterway Training) d\'une équipe de 10 développeurs'
+			]
+		}
+	,
+		{ 'period': { 'start': '2006', 'end': '2007'}
+		, 'company':
+			{ 'name': 'Armadillo'
+			, 'location':
+				{ 'city': 'Vanves'
+				, 'country': 'France'
+				}
+			, 'business': 'Système de gestion de collections numériques'
+			}
+		, 'role': 'Développeur'
+		, 'skills':
+			[ 'SQL' 
+			, 'AJAX'
+			]
+		, 'missions':
+			[ 'adaptation de “Armadillo Média Web” au Web 2.0'
+			]
+		}
+	,
+		{ 'period': { 'start': 'Juin 2005', 'end': '2006'}
+		, 'company':
+			{ 'name': 'Amen France'
+			, 'location':
+				{ 'city': 'Levallois-Perret'
+				, 'country': 'France'
+				}
+			, 'business': 'Service d\'hébergement Internet'
+			}
+		, 'role': 'Technicien de support'
+		, 'skills': []
+		, 'missions':
+			[ 'assistance dans l\'exploitation de serveurs (Microsoft Server OSes or Linux distributions)'
+			, 'gestion des abus en liaison avec les administrateurs système (spam, piratage)'
+			, 'conseil auprès des commerciaux pour qu\'ils conseillent la meilleure offre'
+			, 'documentation pour les utilisateurs et les collaborateurs'
+			, 'formation des nouveaux techniciens aux procédures, produits et outils'
+			]
+		}
+	,
+		{ 'period': { 'start': 'Mars 2005', 'end': 'Mars 2005'}
+		, 'company':
+			{ 'name': 'Amen France'
+			, 'location':
+				{ 'city': 'Levallois'
+				, 'country': 'Irlande'
+				}
+			, 'business': 'Service d\'hébergement Internet'
+			}
+		, 'role': 'Techico-commercial'
+		, 'missions':
+			[ 'ventes des solutions d\'hébergement Internet aux professionnels'
+			]
+		}
+	,
+		{ 'period': { 'start': '2004', 'end': '2005'}
+		, 'company':
+			{ 'name': 'LNA'
+			, 'location':
+				{ 'city': 'Boulogne-Billancourt'
+				, 'country': 'France'
+				}
+			, 'business': 'Vente et location de matériel informatique aux professionnels'
+			}
+		, 'role': 'Développeur'
+		, 'skills':
+			[ 'HTML' 
+			, 'PHP' 
+			, 'Perl' 
+			, 'MySQL'
+			]
+		, 'missions':
+			[ 'développement de maintenance'
+			, 'optimisation des performances du moteur de recherche'
+			, 'développement d\'une application de gestion des impressions (Delphi, MSSQL)'
+			]
+		}
+	,
+		{ 'period': { 'start': '2003', 'end': '2004'}
+		, 'company':
+			{ 'name': 'Com&Com'
+			, 'location':
+				{ 'city': 'Le Plessis-Robinson'
+				, 'country': 'France'
+				}
+			, 'business': 'Éditeur de logiciels de gestions pour la presse'
+			}
+		, 'role': 'Technicien de support, Formateur'
+		, 'skills':
+			[
+			]
+		, 'missions':
+			[ 'support technique par téléphone et courriels'
+			, 'formation des utilisateurs'
+			]
+		}
 	];
+
