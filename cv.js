8711e2361f61b0fb43a0e6a121c15bc888f4be4e
Mickaël Wolff <mickael@lupusmic.org>
2019-08-15 16:04:09 +0200

    Filter out according to starred and level flags

--- cv.js
+++ cv.js
@@ -1,18 +1,44 @@
-assets.xp = assets.xp.map(function (xp)
-{
-	if(xp.company.location.country == assets.id.location.country)
-		delete xp.company.location.country;
+assets.xp = assets.xp
+	.filter(function (xp)
+	{
+		return xp.starred;
+	})
+	.map(function (xp)
+	{
+		if(xp.company.location.country == assets.id.location.country)
+			delete xp.company.location.country;
 
-	return xp;
-});
+		return xp;
+	});
 
-assets.training = assets.training.map(function (training)
-{
-	if(training.organism.location.country == assets.id.location.country)
-		delete training.organism.location.country;
+assets.training = assets.training
+	.map(function (training)
+	{
+		if(training.organism.location.country == assets.id.location.country)
+			delete training.organism.location.country;
 
-	return training;
-});
+		return training;
+	});
+
+assets.skill_set = assets.skill_set
+	.map(function (skill)
+	{
+		if(assets.skill_kinds.soft === skill.kind)
+			return skill;
+
+		skill.content = skill.content
+			.filter(function (one)
+		{
+			return one.level > assets.skill_levels.rookie
+				|| one.level.dev > assets.skill_levels.rookie
+				;
+		});
+		return skill;
+	})
+	.filter(function (skill)
+	{
+		return skill.content && skill.content.length > 0;
+	});
 
 document.title = 'CV de ' + assets.id.name + ', ' + assets.id.role + '.';
 $(document.head)
